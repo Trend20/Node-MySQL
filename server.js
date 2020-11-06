2cd197e9015ed19d375a81da5db21a6e4fe889eb
devenock <enockomondi305@gmail.com>
2020-11-05 21:42:24 -0500

    Addng the CRUD functionality

--- server.js
+++ server.js
@@ -18,10 +18,34 @@ connection.connect((err) => {
 
 // performing queries
 // GETTING DATA
-connection.query('SELECT * FROM users', (err, data) => {
+connection.query('SELECT * FROM users', (err, rows) => {
     console.log('Data received');
-    console.log(data);
+    console.log(rows);
+});
+// loop over the data received
+// rows.forEach((row) => {
+//     console.log(`${row.name} ${row.location}`);
+// })
+
+// CREATING DATA
+const anotherUser = { id: 4, name: 'Jeff', age: 30, location: 'Zambia ' }
+connection.query('INSERT  INTO users SET ?', anotherUser, (err, rows) => {
+    console.log('User added');
+    console.log(anotherUser);
+});
+
+// UPDATING DATA
+connection.query('UPDATE users SET locatio = ? WHERE id = ?', ['North', 5],
+    (err, result) => {
+        console.log('Updated');
+    })
+
+// DELETING DATA
+connection.query('DELETE FROM users WHERE id = ?', [5], (err, result) => {
+    if (err) throw err
+    console.log('Deleted');
 })
+
 
 app.listen(3000, () => {
     console.log('Connected');
